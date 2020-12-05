7801ff7060661482e119bb15f3c92247de73b977
George Munyoro <george.guva@outlook.com>
2020-12-05 15:38:53 +0200

    chore: Format and add missing imports

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -11,13 +11,24 @@ import {
 export const loginUser = async (req: express.Request, res: express.Response) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return handleRequestError(res, [
+        `Missing fields: ${email ? "" : "email, "}${password ? "" : "password"}`,
+      ]);
+    }
+
     const user = await prisma.user.findUnique({
       where: { email },
     });
 
     if (!user) return handleRequestError(res, ["unable to login"]);
 
-    const correctPassword = user.password == createHash("sha256").update(password).digest("base64");
+    const correctPassword =
+      user.password ==
+      createHash("sha256")
+        .update(password)
+        .digest("base64");
     if (!correctPassword) {
       handleRequestError(res, ["incorrect credentials"]);
     }
