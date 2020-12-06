774055d232ce8209884d28fe3c3f40fbc94c7879
Restyled.io <commits@restyled.io>
2020-12-06 16:10:26 +0000

    Restyled by whitespace

--- src/controllers/auth/loggedInUser.ts
+++ src/controllers/auth/loggedInUser.ts
@@ -6,7 +6,7 @@ export const loggedInUser = async (req: express.Request, res: express.Response) 
   try {
     const [success, data] = await getRequestAuthUser(req);
     if (!success) return handleServerError(res, [data.message])
-    
+
     return handleSuccessfulRequest(res, data);
 
   } catch (error) {
